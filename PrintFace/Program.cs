6cd3c5861bbdc936ef5d16c389a7d99e9eb17e30
ilya kozubovskiy <ilyakozubovskiy@outlook.com>
2021-10-14 13:31:11 +0300

    Fixed

--- PrintFace/Program.cs
+++ PrintFace/Program.cs
@@ -1,37 +1,27 @@
-using System;
+﻿using System;
 
 namespace PrintFace
 {
     public static class Program
     {
-        /// <summary>
-        /// Print "Hello, world!" in a separate line.
-        /// </summary>
         public static void Main()
         {
-            Console.WriteLine("  Hello, world! ");
+            Console.WriteLine("Hello, world!");
         }
 
-        /// <summary>
-        /// Print "Hello, userName!" in a separate line.
-        /// </summary>
-        /// <param name="userName">Name of user.</param>
         public static void SayHelloUser(string userName)
         {
-            throw new NotImplementedException();
+            Console.WriteLine("Hello, {0}!", userName);
         }
         
-        /// <summary>
-        /// Print this face.
-        ///   +"""""+
-        ///  (| o o |)
-        ///   |  ^  |
-        ///   | '-' |
-        ///   +-----+
-        /// </summary>
         public static void PrintFace()
         {
-            throw new NotImplementedException();
+            string s = $" +\"\"\"\"\"+{Environment.NewLine}" +
+                               $"(| o o |){Environment.NewLine}" +
+                               $" |  ^  |{Environment.NewLine}" +
+                               $" | '-' |{Environment.NewLine}" +
+                               $" +-----+";
+            Console.WriteLine(s);
         }
     }
 }
